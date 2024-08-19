78b2e307c60b8792f37d37bca1cdbea518886513
thaisvictoria <142370586+thaisvictoria@users.noreply.github.com>
2024-08-19 18:47:34 -0300

    Add files via upload

---
+++ js/aula1.js
@@ -0,0 +1,15 @@
+// js é client-side
+
+//  html/css sao tecnologias de construção de sites
+
+// Origens: Criado por Brendan Eich em 1995 na Netscape.
+// Objetivo inicial: Tornar páginas web mais dinâmicas.
+// Renomeação: De LiveScript para JavaScript.
+// Padronização: Pela ECMA International em 1997.
+// Evolução: Lançamento de várias versões do ECMAScript.
+// Popularidade: Tornou-se uma das linguagens mais utilizadas no mundo.
+// Aplicações: Front-end e back-end de aplicações web.
+// Frameworks e bibliotecas: jQuery, Angular, React, Vue.js.
+// Node.js: Expansão para o desenvolvimento de aplicações de servidor.
+// Ampla utilização: Desenvolvimento web, aplicativos de servidor, computação em nuvem.
+
